b984aed23b203302d99835eebab1ef28db1f2907
isaacBullinger <isaac.k.bullinger@gmail.com>
2024-02-19 11:36:43 -0500

    Does not hang when a draw occurs. Starting with 1 does not make a draw occur

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,7 @@
 import random
+import os
+
+# Known bugs: There is a draw if one is selected.
 
 choice = ''
 computer_choice = ''
@@ -22,27 +25,27 @@ def print_board(display):
 
 def check_all(display):
     global winner
-    if display[0] == display[1] == display[2] and display[0] != ' ':
+    if display[0] == display[1] == display[2] and display[0] != ' ' and display[1] != ' ' and display[2] != ' ':
         winner = display[0]
-    elif display[3] == display[4] == display[5] and display[3] != ' ':
+    elif display[3] == display[4] == display[5] and display[3] != ' ' and display[4] != ' ' and display[5] != ' ':
         winner = display[3]
-    elif display[6] == display[7] == display[8] and display[6] != ' ':
+    elif display[6] == display[7] == display[8] and display[6] != ' ' and display[7] != ' ' and display[8] != ' ':
         winner = display[6]
-    elif display[0] == display[3] == display[6] and display[0] != ' ':
+    elif display[0] == display[3] == display[6] and display[0] != ' ' and display[3] != ' ' and display[6] != ' ':
         winner = display[0]
-    elif display[1] == display[4] == display[7] and display[1] != ' ':
+    elif display[1] == display[4] == display[7] and display[1] != ' ' and display[4] != ' ' and display[7] != ' ':
         winner = display[1]
-    elif display[2] == display[5] == display[8] and display[2] != ' ':
+    elif display[2] == display[5] == display[8] and display[2] != ' ' and display[5] != ' ' and display[8] != ' ':
         winner = display[2]
-    elif display[0] == display[4] == display[8] and display[0] != ' ':
+    elif display[0] == display[4] == display[8] and display[0] != ' ' and display[4] != ' ' and display[8] != ' ':
         winner = display[0]
-    elif display[2] == display[4] == display[6] and display[0] != ' ':
+    elif display[2] == display[4] == display[6] and display[2] != ' ' and display[4] != ' ' and display[6] != ' ':
         winner = display[2]
 
 print('Welcome to tic-tac-toe!')
 print(guide)
 
-while user_point != 'quit' and len(computer_points) < 5 and winner == '' and draw == False:
+while len(user_points) < 5 and winner == '' and draw == False:
 
     while choice == '' or (choice != 'x' and choice != 'o'):
         choice = input('Choose x or o: ')
@@ -51,8 +54,25 @@ while user_point != 'quit' and len(computer_points) < 5 and winner == '' and dra
         else:
             computer_choice = 'x'
 
-    user_point = int(input('Please select where (1-9): '))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print()
+    print_board(display)
+    check_all(display)
+
+    while True:
+        try:
+            user_point = int(input('Please select where (1-9): '))
+        except ValueError:
+            print('Not an integer, try again...')
+            continue
+        else:
+            while user_point < 0 or user_point > 10:
+                print('Please put a number between 1 and 9...')
+                user_point = int(input('Please select where (1-9): '))
+        break
+        
     while user_point == computer_point or user_point in computer_points or computer_point in user_points or user_point in user_points:
+        print('Space taken, please select a blank space...')
         user_point = int(input('Please select where (1-9): '))
 
     user_points.append(user_point)
@@ -61,29 +81,32 @@ while user_point != 'quit' and len(computer_points) < 5 and winner == '' and dra
         if user_point == number:
             display[number - 1] = choice
 
-    if len(user_points) >= 5:
-        draw = True
-        winner = 'draw'
-        print_board(display)
-        check_all(display)
-
     if draw == False:
-        if len(computer_points) < 5:
+        if len(computer_points) < 4:
             computer_point = random.randint(1, 9)
 
-        if len(computer_points) < 5:
+        if len(computer_points) < 4:
             while user_point == computer_point or computer_point in computer_points or computer_point in user_points:
                 computer_point = random.randint(1, 9)
 
-        if len(computer_points) < 5:
+        if len(computer_points) < 4:
             computer_points.append(computer_point)
 
         for number in numbers:
             if computer_point == number:
                 display[number - 1] = computer_choice
-    print()
-    print_board(display)
-    check_all(display)
+
+        print()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print_board(display)
+        check_all(display)
+    else:
+        break
+
+    #print()
+    #os.system('cls' if os.name == 'nt' else 'clear')
+    #print_board(display)
+    #check_all(display)
 
 if (choice == 'x' and winner == 'x') or (choice == 'o' and winner == 'o'):
     who_won = 'user'
@@ -94,7 +117,7 @@ elif (computer_choice == 'x' and winner == 'x') or (computer_choice == 'o' and w
 else:
     who_won = 'draw'
     who_chose = 'no one'
-if winner != 'draw':
+if winner != 'draw' and who_won != 'draw':
     print(f'\nThe winner is {who_won} who chose {who_chose}!')
 else:
     print(f'{who_chose.capitalize()} won, it is a {who_won}...')
